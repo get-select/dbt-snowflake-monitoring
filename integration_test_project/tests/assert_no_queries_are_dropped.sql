2540c7eb810b5ab9d29e824f9a538dfc7abde429
Ian Whitestone <ianjameswhitestone@gmail.com>
2022-10-14 18:15:34 -0400

    Fix failing test

--- integration_test_project/tests/assert_no_queries_are_dropped.sql
+++ integration_test_project/tests/assert_no_queries_are_dropped.sql
@@ -2,21 +2,15 @@
 We expect all queries in query_history to be present in cost_per_query, and vice versa.
 
 Cost per query won't have some of the more recent queries, so we only compare queries
-before the max(start_time) in cost_per_query
+before the current date
 */
-with max_date as (
-    select max(start_time) as latest_start_time
-    from {{ ref('cost_per_query') }}
-)
-
 select *
 from {{ ref('query_history') }} as a
 full outer join {{ ref('cost_per_query') }} as b
     on a.query_id = b.query_id
-cross join max_date
 where
     (
         a.query_id is null
         or b.query_id is null
     )
-    and date(coalesce(a.start_time, b.start_time)) < max_date.latest_start_time
+    and date(coalesce(a.start_time, b.start_time)) < current_date
