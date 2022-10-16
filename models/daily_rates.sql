ebe54af1b30027fabc2f2b8ef44ba544067a66c8
NiallRees <niall@niallrees.com>
2022-10-16 14:22:06 +0100

    Add pre commit config for automatic sqlfluff running

--- models/daily_rates.sql
+++ models/daily_rates.sql
@@ -17,14 +17,14 @@ known day.
 
 with
 dates_base as (
-    select
-        dateadd(
+    select dateadd(
             'day',
             '-' || row_number() over (order by null),
             dateadd('day', '+1', current_date)
         ) as date
-    from table(generator(rowcount => (365*3)))
+    from table(generator(rowcount => (365 * 3)))
 ),
+
 rate_sheet_daily as (
     select
         date,
@@ -34,49 +34,54 @@ rate_sheet_daily as (
         service_type
     from {{ ref('rate_sheet_daily') }}
     where
-        account_locator='{{ current_account }}'
+        account_locator = '{{ current_account }}'
 ),
+
 rates_date_range as (
     select
-        min(date) AS start_date,
-        max(date) AS end_date
+        min(date) as start_date,
+        max(date) as end_date
     from rate_sheet_daily
 ),
+
 rates_date_range_w_usage_types as (
     select
-        start_date,
-        end_date,
+        rates_date_range.start_date,
+        rates_date_range.end_date,
         usage_types.usage_type
     from rates_date_range
     cross join (select distinct usage_type from rate_sheet_daily) as usage_types
 ),
+
 base as (
     select
         db.date,
         dr.usage_type
-    from dates_base AS db
-    inner join rates_date_range_w_usage_types AS dr
+    from dates_base as db
+    inner join rates_date_range_w_usage_types as dr
         on db.date between dr.start_date and dr.end_date
 ),
+
 rates as (
     select
         base.date,
         base.usage_type,
         coalesce(
-            service_type,
-            lag(service_type) ignore nulls over(partition by base.usage_type order by base.date),
-            lead(service_type) ignore nulls over(partition by base.usage_type order by base.date)
+            rate_sheet_daily.service_type,
+            lag(rate_sheet_daily.service_type) ignore nulls over (partition by base.usage_type order by base.date),
+            lead(rate_sheet_daily.service_type) ignore nulls over (partition by base.usage_type order by base.date)
         ) as service_type,
         coalesce(
-            effective_rate,
-            lag(effective_rate) ignore nulls over(partition by base.usage_type order by base.date),
-            lead(effective_rate) ignore nulls over(partition by base.usage_type order by base.date)
+            rate_sheet_daily.effective_rate,
+            lag(rate_sheet_daily.effective_rate) ignore nulls over (partition by base.usage_type order by base.date),
+            lead(rate_sheet_daily.effective_rate) ignore nulls over (partition by base.usage_type order by base.date)
         ) as effective_rate
     from base
-    left join rate_sheet_daily AS rsd
-        on base.date=rsd.date
-        and base.usage_type=rsd.usage_type
+    left join rate_sheet_daily
+        on base.date = rate_sheet_daily.date
+            and base.usage_type = rate_sheet_daily.usage_type
 )
+
 select *
 from rates
 order by date
