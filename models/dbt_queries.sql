90d8c5b583e477e3e3d7c51b269aefac866f6629
NiallRees <niall@niallrees.com>
2022-12-21 11:25:06 +0000

    Add dbt_queries model

---
+++ models/dbt_queries.sql
@@ -0,0 +1,25 @@
+select
+    dbt_metadata['dbt_version']::string as dbt_version,
+    dbt_metadata['target_name']::string as dbt_target_name,
+    dbt_metadata['target_database']::string as dbt_target_database,
+    dbt_metadata['target_schema']::string as dbt_target_schema,
+    dbt_metadata['invocation_id']::string as dbt_invocation_id,
+    dbt_metadata['node_id']::string as dbt_node_id,
+    dbt_metadata['node_resource_type']::string as dbt_node_resource_type,
+    dbt_metadata['materialized']::string as dbt_node_materialized,
+    dbt_metadata['is_incremental']::string as dbt_node_is_incremental,
+    dbt_metadata['dbt_cloud_project_id']::string as dbt_cloud_project_id,
+    dbt_metadata['dbt_cloud_job_id']::string as dbt_cloud_job_id,
+    dbt_metadata['dbt_cloud_run_id']::string as dbt_cloud_run_id,
+    dbt_metadata['dbt_cloud_run_reason_category']::string as dbt_cloud_run_reason_category,
+    dbt_metadata['dbt_cloud_run_reason']::string as dbt_cloud_run_reason,
+    {% if var('dbt_cloud_account_id', none) -%}
+    'https://cloud.getdbt.com/next/deploy/' || '{{ var('dbt_cloud_account_id') }}' || '/projects/' || dbt_cloud_project_id || '/jobs/' || dbt_cloud_job_id as dbt_cloud_job_url,
+    'https://cloud.getdbt.com/next/deploy/' || '{{ var('dbt_cloud_account_id') }}' || '/projects/' || dbt_cloud_project_id || '/runs/' || dbt_cloud_run_id as dbt_cloud_run_url,
+    {%- else -%}
+    'Required dbt_cloud_account_id variable not set' as dbt_cloud_job_url,
+    'Required dbt_cloud_account_id variable not set' as dbt_cloud_run_url,
+    {%- endif %}
+    *
+from {{ ref('query_history_enriched') }}
+where dbt_metadata is not null
