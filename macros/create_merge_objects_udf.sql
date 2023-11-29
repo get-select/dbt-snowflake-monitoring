505262254eb09186516cf7829cff7de5411c4cd8
Ernesto Ongaro <ernestoongaro@gmail.com>
2023-11-29 21:09:13 +0000

    respect quoting in this macro

--- macros/create_merge_objects_udf.sql
+++ macros/create_merge_objects_udf.sql
@@ -1,6 +1,6 @@
 {% macro create_merge_objects_udf(relation) %}
 
-create or replace function {{ relation.database }}.{{ relation.schema }}.merge_objects(obj1 variant, obj2 variant)
+create or replace function {{ adapter.quote_as_configured(this.database, 'database') }}.{{  adapter.quote_as_configured(this.schema, 'schema') }}.merge_objects(obj1 variant, obj2 variant)
 returns variant
 language javascript
 comment = 'Created by dbt-snowflake-monitoring dbt package.'
