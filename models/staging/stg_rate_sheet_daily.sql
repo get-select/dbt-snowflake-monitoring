ebeb05c6b37bcfa30585e9b737da3d4f215c2c3b
Ian Whitestone <ianjameswhitestone@gmail.com>
2024-01-15 16:28:40 -0500

    Add handling for CLOUD_SERVICES service_type (#143)
    * Add handling for CLOUD_SERVICES service_type
    
    * Add changelog

--- models/staging/stg_rate_sheet_daily.sql
+++ models/staging/stg_rate_sheet_daily.sql
@@ -13,10 +13,12 @@ select
     currency,
     effective_rate,
     case
-        -- Have only seen this on one account. Normally it is COMPUTE, and all our downstream models rely on that
-        -- May adjust this in the future if Snowflake is permanently changing these fields for all accounts
-        when service_type = 'WAREHOUSE_METERING' then 'COMPUTE'
-        else service_type
+        -- For most Snowflake accounts, the service_type field is always COMPUTE or STORAGE
+        -- Have recently seen new values introduced for one account: WAREHOUSE_METERING and CLOUD_SERVICES
+        -- For now, we'll force these to either be COMPUTE or STORAGE since that's what the downstream models expect
+        -- May adjust this in the future if Snowflake is permanently changing these fields for all accounts and starts offering different credit rates per usage_type
+        when service_type = 'STORAGE' then 'STORAGE'
+        else 'COMPUTE'
     end as service_type
 from {{ source('snowflake_organization_usage', 'rate_sheet_daily') }}
 order by date
