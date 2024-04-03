798d7d1814fd26d850b59a41acedc747e6aef6d1
Simo Tumelius <simo.tumelius@gmail.com>
2024-04-03 08:41:22 +0300

    Undo service_type transformation in stg_rate_sheet_daily

--- models/staging/stg_rate_sheet_daily.sql
+++ models/staging/stg_rate_sheet_daily.sql
@@ -12,13 +12,6 @@ select
     usage_type,
     currency,
     effective_rate,
-    case
-        -- For most Snowflake accounts, the service_type field is always COMPUTE or STORAGE
-        -- Have recently seen new values introduced for one account: WAREHOUSE_METERING and CLOUD_SERVICES
-        -- For now, we'll force these to either be COMPUTE or STORAGE since that's what the downstream models expect
-        -- May adjust this in the future if Snowflake is permanently changing these fields for all accounts and starts offering different credit rates per usage_type
-        when service_type = 'STORAGE' then 'STORAGE'
-        else 'COMPUTE'
-    end as service_type
+    service_type
 from {{ source('snowflake_organization_usage', 'rate_sheet_daily') }}
 order by date
