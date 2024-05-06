9a74d5af1b3cdaaa760a93f9ae587e0016043b13
Fernando Brito <fernando@select.dev>
2024-05-06 12:23:40 +0200

    Improve service spend coverage on the hourly_spend model

--- models/staging/stg_database_storage_usage_history.sql
+++ models/staging/stg_database_storage_usage_history.sql
@@ -4,5 +4,6 @@ select
     usage_date as date,
     database_name,
     average_database_bytes,
-    average_failsafe_bytes
+    average_failsafe_bytes,
+    average_hybrid_table_storage_bytes
 from {{ source('snowflake_account_usage', 'database_storage_usage_history') }}
