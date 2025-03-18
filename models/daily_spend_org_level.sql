fdd0501a150fdf42f7830226dd35e2d796a3bb38
Anna Bisset <annabisset@canva.com>
2025-03-19 10:59:51 +1100

    create org level daily_spend model

--- models/daily_spend_org_level.sql
+++ models/daily_spend_org_level.sql
@@ -1,9 +1,12 @@
-{{
-config(
-    enabled=not(var('uses_org_view', false)))
-}}
+{{ config(
+    enabled=var('uses_org_view', false),
+    alias='daily_spend'
+) }}
 select
     convert_timezone('UTC', hour)::date as date, -- get UTC date to align with Snowflake billing
+    organization_name,
+    account_name,
+    account_locator,
     service,
     storage_type,
     warehouse_name,
@@ -11,5 +14,5 @@ select
     sum(spend) as spend,
     sum(spend_net_cloud_services) as spend_net_cloud_services,
     any_value(currency) as currency
-from {{ ref('hourly_spend') }}
-group by 1, 2, 3, 4, 5
+from {{ ref('hourly_spend_org_level') }}
+group by 1, 2, 3, 4, 5, 6, 7, 8
